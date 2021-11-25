298f5a485688992f2bc793856a32b7ab2b9b2820
Richie Yoseph Wijaya <richieyoseph@gmail.com>
2021-11-25 18:39:03 +0700

    add Task create page

--- GUI/Task.php
+++ GUI/Task.php
@@ -124,62 +124,64 @@ use EverydayTasks\Util;
 //        ]);
 //    }, ['get', 'post']);
 
-//    /*
-//     * Add an activity
-//     */
-//    Route::add('/add', function(){
-//        $category_list = Category::getAll(Util::$db);
-//
-//        // create base activity
-//        $activity = new Activity(
-//            Util::$db,
-//            bin2hex(random_bytes(4)),
-//            '',
-//            '',
-//            new DateTime(),
-//            null
-//        );
-//
-//        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//            $array = $_POST;
-//            // subject and description cannot be empty
-//            if (
-//                empty($array['subject']) ||
-//                empty($array['description'])
-//            ) {
-//                Template::view('Templates/activities_edit.html', [
-//                    'page_title' => 'Activity edit',
-//                    'page_heading' => 'New Activity',
-//                    'activity' => $activity,
-//                    'category_list' => $category_list,
-//                    'notifications' => [
-//                        ['type'=>'error', 'message'=>'Subject or description must not be empty']
-//                    ]
-//                ]);
-//                return;
-//            }
-//
-//            // category is optional
-//            if (array_key_exists('category', $array)) {
-//                $activity->setCategory(
-//                    Category::searchById(Util::$db, $array['category'])
-//                );
-//            }
-//
-//            $activity->setSubject(Util::sanitize($array['subject']));
-//            $activity->setDescription(Util::sanitize($array['description']));
-//            $activity->addToDatabase();
-//            header('Location: /activity/');
-//            return;
-//        }
-//
-//        Template::view('Templates/activities_edit.html', [
-//            'page_title' => 'Activity edit',
-//            'page_heading' => 'Edit Activity',
-//            'activity' => $activity,
-//            'category_list' => $category_list
-//        ]);
-//    }, ['get', 'post']);
+/*
+* Add a task
+*/
+Route::add('/add', function(){
+   $category_list = Category::getAll(Util::$db);
+
+   // create base task
+   $task = new Task(
+       Util::$db,
+       bin2hex(random_bytes(4)),
+       '',
+       '',
+       null,
+       null,
+       null
+   );
+
+   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+       $array = $_POST;
+       // subject cannot be empty
+       if (empty($array['subject'])) {
+           Template::view('Templates/tasks_edit.html', [
+               'page_title' => 'Tasks edit',
+               'page_heading' => 'New Task',
+               'task' => $task,
+               'category_list' => $category_list,
+               'notifications' => [
+                   ['type'=>'error', 'message'=>'Subject or description must not be empty']
+               ]
+           ]);
+           return;
+       }
+
+       // category is optional
+       if (array_key_exists('category', $array)) {
+           $task->setCategory(
+               Category::searchById(Util::$db, $array['category'])
+           );
+       }
+
+       if (!empty($array["due"])) {
+           $task->due = new DateTime($array["due"]);
+       }
+
+       $task->setSubject(Util::sanitize($array['subject']));
+       $task->setDescription(Util::sanitize($array['description']));
+       $task->addToDatabase();
+       header('Location: /task/');
+       return;
+   }
+
+    Template::view('Templates/tasks_edit.html', [
+        'page_title' => 'Tasks edit',
+        'page_heading' => 'New Task',
+        'task' => $task,
+        'category_list' => $category_list
+    ]);
+}, ['get', 'post']);
 
 
     // Execute GUI route
