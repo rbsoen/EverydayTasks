1fd1008180186dfe7d4577e4f52a260fe9b73022
Rodriguez Breil Soenoto <rbsoenoto@gmail.com>
2021-11-25 16:29:59 +0700

    (index) Add Task models and API

--- index.php
+++ index.php
@@ -1,33 +1,35 @@
 <?php
-    // Only for debugging
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
+// Only for debugging
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-    // configuration
-    require_once 'config.php';
+// configuration
+require_once 'config.php';
 
-    require_once 'Library/Util.php'; // various helper functions
-    require_once 'Library/ResponseCode.php'; // response code constants
-    require_once 'Library/Idempotency.php'; // idempotency feature
+require_once 'Library/Util.php'; // various helper functions
+require_once 'Library/ResponseCode.php'; // response code constants
+require_once 'Library/Idempotency.php'; // idempotency feature
 
-    // components
-    require_once 'Models/Activity.php';
-    require_once 'Models/Category.php';
+// components
+require_once 'Models/Activity.php';
+require_once 'Models/Category.php';
+require_once 'Models/Task.php';
 
-    // Routing and Templating
-	require_once 'Library/External/Route.php';
-	require_once 'Library/External/Template.php';
+// Routing and Templating
+require_once 'Library/External/Route.php';
+require_once 'Library/External/Template.php';
 
-    // Use library
-    use Steampixel\Route;
-    use EverydayTasks\Activity;
-    use EverydayTasks\Util;
-    use EverydayTasks\Idempotency;
+// Use library
+use Steampixel\Route;
+use EverydayTasks\Activity;
+use EverydayTasks\Util;
+use EverydayTasks\Idempotency;
 
-    // API
-    require_once 'API/Activity.php';
-    require_once 'API/Category.php';
+// API
+require_once 'API/Activity.php';
+require_once 'API/Category.php';
+require_once 'API/Task.php';
 
-    // GUI
-    require_once 'GUI/Activity.php';
+// GUI
+require_once 'GUI/Activity.php';
 ?>
